064eca8da9643d76da959643b1adaf7270a50af6
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-04-29 11:14:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+function  mapArray(str) {
+	let arr = [];
+	let map = new Map();
+	for(let t of str){
+		if(map.has(t)){
+			map.set(t,(arr.push(indexOf(map.get(t)))
+		}else{
+			map.set(t,indexOf(map.get(t)));
+		}
+	}
+	return map;
+}
+let str = "dodo";
+console.log(mapArray(str));
